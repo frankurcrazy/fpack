ba9743b6f9cf7106663a5ceed231ee3f6e43201a
Frank Chang <frank@csie.io>
2020-08-25 18:01:21 +0800

    Bump version: 0.1.1 → 1.0.0

--- fpack/__init__.py
+++ fpack/__init__.py
@@ -4,7 +4,7 @@
 from fpack.fields import *
 from fpack.msg import *
 
-__version__ = "0.1.1"
+__version__ = "1.0.0"
 __author__ = "Frank Chang"
 __author_email__ = "frank@csie.io"
 __license__ = "BSD"
